7310a0cf127580808a693f419922c5f46960997d
urakubo <hurakubo@gmail.com>
2021-05-17 13:32:00 +0900

    Update sim_tw.m

--- funcs2/sim_tw.m
+++ funcs2/sim_tw.m
@@ -8,6 +8,7 @@ function [sd, sd_noDAdip, sd_noCa] = sim_tw(model, DA_delay, container)
 	d_DA   = container('duration_DA');
 	Toffset_VGCC = T_VGCC.Value;
 
+	reserv_      = T_DA.Value;
 	num_DA_delay = numel(DA_delay);
 	sd   = cell(num_DA_delay, 1);
 	for i = 1:num_DA_delay;
@@ -15,14 +16,15 @@ function [sd, sd_noDAdip, sd_noCa] = sim_tw(model, DA_delay, container)
 		T_DA.Value = DA_delay(i) + Toffset_VGCC;
 		sd{i,1} = sbiosimulate(model);
 	end;
+	T_DA.Value   = reserv_;
 
 	reserv_      = d_DA.Value;
 	d_DA.Value   = 0.2;
 	sd_noDAdip   = sbiosimulate(model);
 	d_DA.Value   = reserv_;
 
-	stop_time = get(getconfigset(model), 'StopTime');
 	reserv_      = T_VGCC.Value;
+	stop_time = get(getconfigset(model), 'StopTime');
 	T_VGCC.Value = stop_time + 1;
 	sd_noCa      = sbiosimulate(model);
 	T_VGCC.Value = reserv_;
